319dacd4343323bb6879f44a7f4cb03a659c00c3
PervezZz <63874062+nicholaspervez@users.noreply.github.com>
2023-03-01 15:53:33 +0000

    added start script

--- cron.js
+++ cron.js
@@ -1,28 +1,3 @@
-// import { schedule } from 'node-cron';
-// import axios from 'axios';
-
-// async function baga(){
-//     console.log('fired');
-//     const uid = '303Ut9TLrAQjyq5hlJrmlsB66Tl2';
-//     const todaysFood = await fetch(
-//         `https://nicks-branch-v1million.onrender.com/todaysFood/${uid}`
-//       );
-//       const data = await todaysFood.json();
-//       const food = data.payload;
-//      axios.post(`https://app.nativenotify.com/api/indie/notification`, {
-//       subID: `${uid}`,
-//       appId: 6107,
-//       appToken: 'RscfdJGUHSLru74JJd6STe',
-//       title: 'Food due to expire today...',
-//       message: `You have ${food.length} items going off today, better eat em up!`
-//  });
-//     }
-
-//     schedule('* * * * * ', async() => baga(), {
-//       scheduled: true,
-//       timezone: "Europe/London"
-//     });
-
 const backendURL = "https://spoiler-alert-backend.onrender.com";
 
 import { schedule } from "node-cron";
